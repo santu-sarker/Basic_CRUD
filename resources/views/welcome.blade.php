454878aa233ed880292dec5efe648f1f0dba0f8d
santu-sarker <mdsantusarker@gmail.com>
2021-11-08 23:49:37 +0600

    commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>@yield('title')</title>
+        {{-- favicon section --}}
+        <link rel="icon" href=" {{ url('favicon.png') }}">
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
+    </head>
+    <body class="antialiased">
+        <button class="btn btn-primary"> hello there</button>
+    </body>
+</html>
